142c7017b7755a8a355a25c2939946b04dba2efa
mrasul7 <malyasayev@bk.ru>
2025-11-24 20:30:51 +0300

    Добавлен файл cli.py с помощью которого создается пользователь с ролью superadmin

---
+++ cli.py
@@ -0,0 +1,74 @@
+# cli.py
+import asyncio
+import re
+import rich
+import bcrypt
+import typer
+from db.database import async_session_maker
+from db.models import User
+from sqlalchemy import select, delete 
+from pydantic import BaseModel, EmailStr, ValidationError
+
+class EmailValidate(BaseModel):
+    email: EmailStr
+    
+
+app = typer.Typer()
+
+async def superadmin_create(username, email, password, confirm_password):
+    async with async_session_maker() as session:
+        try:
+            EmailValidate(email=email)
+            
+            hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+            db_superadmin = User(
+                username=username,
+                password=hashed_password,
+                email=email,
+                role="superadmin"
+            )
+            if password != confirm_password:
+                rich.print(f"[red][bold]Passwords must be the same! Try again[/bold][/red]")
+                return
+            
+            result = await session.execute(select(User).where(User.role == "superadmin"))
+            if result.scalar_one_or_none():
+                rich.print(f"[red][bold]Superadmin is already created![/bold][/red]")
+                rich.print(f"[purple][bold]Do you want to delete the previous superadmin and create a new one?(y/n)[/bold][/purple]: ")
+                answer = input().strip().lower()
+                if answer != "y":
+                    rich.print(f"[yellow][bold]Operation cancelled.[/bold][/yellow]")
+                    return
+                
+                await session.execute(delete(User).where(User.role == "superadmin"))
+                await session.commit()
+                
+                session.add(db_superadmin)
+                await session.commit()
+                rich.print(f"[green][bold]The previous superadmin was removed and a new one was added with {username=} and {email=}[/bold][/green]")
+                
+            
+            session.add(db_superadmin)
+            await session.commit()
+            rich.print(f"[green][bold]Superadmin created successfully![/bold][/green]")
+        except ValidationError:
+            rich.print(f"[red][bold]Invalid email! Try again![/bold][/red]")
+            typer.echo("Invalid email")
+        except Exception as e:
+            await session.rollback()
+            rich.print(f"[red][bold]Error {e}[/bold][/red]")
+        finally:
+            await session.close()
+
+@app.command()
+def create_superadmin(
+    username = typer.Argument(..., help="Username for superadmin: "),
+    email: str = typer.Argument(..., help="Email for superadmin: "),
+    password = typer.Argument(..., help="Password for superadmin: "),
+    confirm_password = typer.Argument(..., help="Confirm password: ")
+):
+    
+    asyncio.run(superadmin_create(username, email, password, confirm_password))
+
+if __name__ == "__main__":
+    app()
