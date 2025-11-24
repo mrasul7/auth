c0d040ee86a1012c13178d96bc31db3954637c9f
mrasul7 <malyasayev@bk.ru>
2025-11-24 20:29:46 +0300

    Добавлены модели pydantic для users и items

---
+++ schemas/items.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class ItemCreate(BaseModel):
+    name: str
+    description: str
+    
+class ItemResponse(ItemCreate):
+    id: int
+    
+
+class Item(ItemCreate):
+    id: int
+    owner_id: int 
+    
+
+class ItemUpdate(BaseModel):
+    name: str | None = None 
+    description: str | None = None
+    
---
+++ schemas/users.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict, EmailStr
+    
+
+class UserRegister(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    confirm_password: str
+    
+    
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+    
+
+class UserUpdate(BaseModel):
+    new_username: str | None = None 
+    new_email: str | None = None
+    
+    model_config = ConfigDict(from_attributes=True)
