68fc48263098ebd785faba4434141c0051afd7b6
mrasul7 <malyasayev@bk.ru>
2025-11-24 20:31:52 +0300

    Добавлен файл config.py с настройками jwt и db

---
+++ config.py
@@ -0,0 +1,15 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_URL: str 
+    JWT_SECRET_KEY: str 
+    JWT_ALGORITHM: str 
+    
+    model_config = SettingsConfigDict(
+        env_file=".env"
+    )
+    
+    
+settings = Settings()
